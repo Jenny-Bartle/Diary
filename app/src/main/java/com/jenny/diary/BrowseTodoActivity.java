c40c8b990611eab79cd09af88592280cdc72d228
Jennycide <jgbartle@gmail.com>
2015-08-04 22:37:34 +0100

    fix opening of todo browse. Dates display incorrectly

--- app/src/main/java/com/jenny/diary/BrowseTodoActivity.java
+++ app/src/main/java/com/jenny/diary/BrowseTodoActivity.java
@@ -6,7 +6,6 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.LinearLayout;
-import android.widget.TableLayout;
 
 import java.util.List;
 
@@ -20,15 +19,15 @@ public class BrowseTodoActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         TaskDatabaseHandler db = new TaskDatabaseHandler(this);
         List<Task> tasks = db.getAllTasks();
-        TableLayout todosContainer = new TableLayout(this);
-        for (Task task : tasks) {
-            TodoBrowseElement todo = new TodoBrowseElement(this, task);
-            todosContainer.addView(todo);
-        }
         LayoutInflater li = LayoutInflater.from(this);
-        LinearLayout todosView = (LinearLayout)li.inflate(R.layout.todo_browse_list_row, null);
-        todosView.addView(todosContainer);
+        LinearLayout todosView = (LinearLayout)li.inflate(R.layout.activity_browse_todos, null);
         setContentView(todosView);
+
+        for (Task task : tasks) {
+            TodoBrowseElement row = (TodoBrowseElement)li.inflate(R.layout.todo_browse_list_row, null);
+            row.setTextValues(task);
+            todosView.addView(row);
+        }
         System.out.println(todosView.getChildAt(0));
     }
 
--- app/src/main/java/com/jenny/diary/TodoBrowseElement.java
+++ app/src/main/java/com/jenny/diary/TodoBrowseElement.java
@@ -1,6 +1,8 @@
 package com.jenny.diary;
 
 import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
 import android.widget.TableRow;
 import android.widget.TextView;
 
@@ -11,8 +13,18 @@ import java.util.Date;
  */
 public class TodoBrowseElement extends TableRow {
 
-    public TodoBrowseElement(Context context, Task task) {
+    public TodoBrowseElement(Context context, AttributeSet attributeSet) {
+        super(context, attributeSet);
+    }
+
+    public TodoBrowseElement(Context context) {
         super(context);
+    }
+
+    public void setTextValues(Task task) {
+        LayoutInflater inflater = LayoutInflater.from(getContext());
+        //RelativeLayout layout = (RelativeLayout) inflater.inflate(R.layout.todo_browse_list_row, context);
+
         TextView title = (TextView)findViewById(R.id.todo_browse_list_row_header);
         TextView date = (TextView)findViewById(R.id.todo_browse_list_row_time);
 
